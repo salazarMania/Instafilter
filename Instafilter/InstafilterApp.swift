0240a146b44320c384ac345eeae0259f59a53cec
saniya <saniyaaaaakk@gmail.con>
2024-08-20 14:01:34 +0530

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by SANIYA KHATARKAR on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
